b6a892a4dfc4a82601be8b7469e78a7ac297d6c7
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-06-21 22:56:25 -0300

    Refactor move foundations into folder and start  new lessons + progress

--- Foundations/Ocean.cs
+++ Foundations/Ocean.cs
@@ -4,9 +4,8 @@ public partial class Ocean : Node2D
 {
 	[Export] private Sprite2D _planeSprite;
 	[Export] private Sprite2D _helicoperSprite;
-
-	private float _rotationDir = 0.0f;
 	private const float PLANESPEED = 100.0f;
+	private const float PLANEROTATIONSPEED = 3.0f;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -19,15 +18,18 @@ public partial class Ocean : Node2D
 	{
 		if(Input.IsActionPressed("ui_up"))
 			_planeSprite.MoveLocalX(PLANESPEED * (float)delta);
-			
-		// rotate right
-		_rotationDir = 0;
-		if (Input.IsActionPressed("ui_right"))
-			_rotationDir = 1;
-		// rotate left
-		if(Input.IsActionPressed("ui_left"))
-			_rotationDir = -1;
 
-		_planeSprite.GlobalRotation += _rotationDir * 5f * (float)delta;
+
+		_planeSprite.GlobalRotation += GetPlayerRotarionDirection() * PLANEROTATIONSPEED * (float)delta;
+	}
+
+	private static float GetPlayerRotarionDirection()
+	{
+		if (Input.IsActionPressed("ui_right"))
+			return 1;
+		if(Input.IsActionPressed("ui_left"))
+			return -1;
+
+		return 0;
 	}
 }
---
+++ GodoCSharp/Scenes/World/World.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class World : Node2D
+{
+	public override void _EnterTree()
+	{
+		
+	}
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+	public override void _ExitTree()
+	{
+		
+	}
+}
